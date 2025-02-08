e8b41256e406ca81e7d2c8bf08dea59c67dd18d9
Rafael Delgado <oliveirarafael22@outlook.com.br>
2025-02-08 14:10:45 -0300

    feat UserController
    Responsavel sobre os dados do usuario

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    use AuthorizesRequests;
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        try {
+        $this->authorize('viewAny', User::class);
+        }catch (AuthorizationException $e) {
+            return response()->json(['error' => $e->getMessage()], 403);
+
+        }
+        return 'Hello from UserController@index';
+    }
+
+
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        return 'Hello from UserController@index';
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        return 'Hello from UserController@index';
+    }
+
+
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        return 'Hello from UserController@index';
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        return 'Hello from UserController@index';
+    }
+}

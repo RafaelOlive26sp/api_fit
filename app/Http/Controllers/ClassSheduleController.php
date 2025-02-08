deb17a9c4c2f8e182a4809f78aadd9dc9d61ff94
Rafael Delgado <oliveirarafael22@outlook.com.br>
2025-02-08 16:16:11 -0300

    feat controllerClass

---
+++ app/Http/Controllers/ClassSheduleController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\ClassSchedulesResquest;
+use App\Models\ClassSchedule;
+use App\Models\User;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Http\Request;
+
+class ClassSheduleController extends Controller
+{
+    use AuthorizesRequests;
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return 'ClassSheduleController@index';
+    }
+
+
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(ClassSchedulesResquest $request)
+    {
+        $this->authorize('create', User::class);
+        $validateData = $request->validated();
+
+        return ClassSchedule::create($validateData);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}

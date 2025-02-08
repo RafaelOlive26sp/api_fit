9bc551b1ae90ad853ec87f3ea4d9549e4bd73a8a
Rafael Delgado <oliveirarafael22@outlook.com.br>
2025-02-08 16:11:04 -0300

    feat routes
    cricação de rotas para os metodos

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,21 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ClasseController;
+use App\Http\Controllers\ClassSheduleController;
+use App\Http\Controllers\PaymentController;
+use App\Http\Controllers\StudentController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function (){
 Route::resource('users', UserController::class)->middleware('auth:sanctum');
+Route::resource('classes', ClasseController::class)->middleware('auth:sanctum');
+Route::resource('classes/schedules', ClassSheduleController::class)->middleware('auth:sanctum');
+Route::resource('student', StudentController::class)->middleware('auth:sanctum');
+Route::resource('payment', PaymentController::class)->middleware('auth:sanctum');
+
 });
 
 Route::post('login', [AuthController::class, 'login']);

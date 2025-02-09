765ade44de3b4c3d921f093bb0b8d5cb3d8bc74a
Rafael <oliveirarafael22@outlook.com.br>
2025-02-09 04:16:56 -0300

    feat: show
    implementado a logica para retorno dos dados, porem faltou implementar e testa a policy

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -4,11 +4,15 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\AppointmentRequest;
+use App\Http\Resources\AppointmentResource;
 use App\Models\Appointment;
+use App\Models\Student;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Http\Request;
 
 class AppointmentController extends Controller
 {
+    use AuthorizesRequests;
     /**
      * Display a listing of the resource.
      */
@@ -33,7 +37,19 @@ class AppointmentController extends Controller
      */
     public function show(string $id)
     {
-        //
+        // $userId = auth()->user()->id;
+        // $studentUser = Student::where('users_id', $userId);
+
+        // $this->authorize('view', $studentUser);
+        // Falta configurar a Policy
+
+
+         return new AppointmentResource(
+            Appointment::where('id', $id)
+            ->with(['class_schedule.classe', 'payment', 'student'])
+            ->firstOr(fn() => abort(404, 'Nenhum agendamento encontrado.'))
+         );
+
     }
 
 

31a6318fd7c0829640a72fd3f3bb2e3df7af6b2e
Rafael <oliveirarafael22@outlook.com.br>
2025-02-15 04:20:06 -0300

    refactor: metodo login
    o metodo login foi rafatorado para retornar alguns dados do usuario como nome e se o cadastro na tabela de aluno esta preenchida

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Student;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use function Pest\Laravel\json;
 
 class AuthController extends Controller
 {
@@ -13,13 +16,41 @@ class AuthController extends Controller
         if (Auth::attempt($credentials)) {
            $user = Auth::user();
            $token = $user->createToken('auth_token')->plainTextToken;
-           return response()->json([
-               'access_token' => $token,
-               'token_type' => 'Bearer',
-           ]);
+           $isStudent = Student::where('users_id',$user->id)->exists();
+
+            if ($isStudent)
+            {
+                return response()->json([
+                    'access_token' => $token,
+                    'token_type' => 'Bearer',
+                    'user'=>[
+                        'name'=> $user->name,
+                        'CompleteStudentRecord'=>$isStudent
+                    ]
+                ]);
+            }else
+            {
+                return response()->json([
+                    'access_token' => $token,
+                    'token_type' => 'Bearer',
+                    'user'=>[
+                        'name'=> $user->name,
+                        'CompleteStudentRecord'=>$isStudent
+                    ]
+                ]);
+            }
+
+
         }
         return  response()->json([
             'message' => 'Unauthorized'
         ], 401);
+    }
+    public function logout(Request $request):JsonResponse
+    {
+        $request->user()->tokens()->delete();
+        return response()->json([
+            'message'=>'Logged out'
+        ]);
     }
 }

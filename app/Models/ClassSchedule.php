d335c48c1f611fb2baf5a89b8b4165e0954085d9
Rafael <oliveirarafael22@outlook.com.br>
2025-02-19 02:56:37 -0300

    refactor:  campo date

--- app/Models/ClassSchedule.php
+++ app/Models/ClassSchedule.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class ClassSchedule extends Model
 {
   protected $fillable = [
-      'day_of_week','start_time','end_time','classes_id'
+      'day_of_week','start_time','end_time','classes_id','date'
   ];
   public function classe()
   {

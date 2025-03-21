3b059fbe78e90943bd001fe4d474e3e91ea54a2a
Rafael <oliveirarafael22@outlook.com.br>
2025-03-21 03:15:54 -0300

    refactor: update delete method in UserPolicy to allow admin and teacher roles

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -45,7 +45,7 @@ class UserPolicy
      */
     public function delete(User $user, User $model): bool
     {
-        return false;
+        return in_array($user->role, ['admin', 'teacher']);
     }
 
     /**

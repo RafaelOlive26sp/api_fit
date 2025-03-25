ceabc12c741781085ad5291bd1f87ec5002346d4
Rafael <oliveirarafael22@outlook.com.br>
2025-03-24 23:24:59 -0300

    feat: update DatabaseSeeder to generate random student users

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Nette\Utils\Random;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,24 +16,26 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-        'name' => 'Admin Rafael',
-        'email' => 'admin@example.com',
-        'password' => bcrypt('password'),
-        'role' => 'admin',
-        ]);
-        User::factory()->create([
-        'name' => 'Teacher Daniela',
-        'email' => 'teacher@example.com',
-        'password' => bcrypt('password'),
-        'role' => 'teacher',
-        ]);
+        // User::factory()->create([
+        // 'name' => 'Admin Rafael',
+        // 'email' => 'admin@example.com',
+        // 'password' => bcrypt('password'),
+        // 'role' => 'admin',
+        // ]);
+        // User::factory()->create([
+        // 'name' => 'Teacher Daniela',
+        // 'email' => 'teacher@example.com',
+        // 'password' => bcrypt('password'),
+        // 'role' => 'teacher',
+        // ]);
 
-        User::factory()->create([
-            'name' => 'Juliana',
-            'email' => 'Juliana@example.com',
+        for ($i = 0; $i < 10; $i++) {
+            User::factory()->create([
+            'name' => Random::generate(10),
+            'email' => Random::generate(10) . '@example.com',
             'password' => bcrypt('password'),
             'role' => 'student',
-        ]);
+            ]);
+        }
     }
 }

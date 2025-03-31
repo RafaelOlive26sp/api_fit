178f87ec7c884a40edf59c1afbab21aaef2abefd
Rafael <oliveirarafael22@outlook.com.br>
2025-03-31 03:45:23 -0300

    fix: correct array formatting in ClassScheduleResource to ensure proper JSON structure

--- app/Http/Resources/ClassScheduleResource.php
+++ app/Http/Resources/ClassScheduleResource.php
@@ -15,7 +15,7 @@ class ClassScheduleResource extends JsonResource
     public function toArray(Request $request): array
     {
         return
-        [
+        [   'id'=> $this->id,
             'date'=> $this->date,
             'day_of_week'=> $this->day_of_week,
             'start_time'=> $this->start_time,

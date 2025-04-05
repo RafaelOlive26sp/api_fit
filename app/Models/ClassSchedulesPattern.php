2feaa5b84cb04a26de88d39ed6d407182cd36b46
Rafael Delgado <oliveirarafael22@outlook.com.br>
2025-04-04 21:24:08 -0300

    refactor:
    ajuste do nome

--- app/Models/ClassSchedulesPattern.php
+++ app/Models/ClassSchedulesPattern.php
@@ -9,7 +9,7 @@ class ClassSchedulesPattern extends Model
     protected $fillable = [
         'classes_id', 'day_of_week', 'start_time', 'end_time'
     ];
-    public function class_schedule()
+    public function class()
     {
         return $this->belongsTo(Classe::class, 'classes_id', 'id');
     }

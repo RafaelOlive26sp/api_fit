ac8b49891e0c94d81838af6e5782d15d4e5a7d8b
Rafael <oliveirarafael22@outlook.com.br>
2025-04-14 03:32:48 -0300

    feat: add user relationship to Classe model

--- app/Models/Classe.php
+++ app/Models/Classe.php
@@ -39,4 +39,8 @@ class Classe extends Model
    {
        return $this->hasMany(StudentClass::class, 'classes_id', 'id');
    }
+   public function user()
+   {
+       return $this->hasMany(User::class, 'id');
+   }
 }

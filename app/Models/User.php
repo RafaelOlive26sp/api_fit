e3566b2a4dab1d53d68519855eb1607e87753133
Rafael <oliveirarafael22@outlook.com.br>
2025-04-14 03:32:57 -0300

    feat: add classes relationship to User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -51,4 +51,8 @@ class User extends Authenticatable
     {
         return $this->hasOne(Student::class,'users_id');
     }
+    public function classes()
+    {
+        return $this->hasOne(Classe::class,'id');
+    }
 }

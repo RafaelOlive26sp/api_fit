ef403089d3ea9d88fa0fdaa0edaadf8787edc23c
Rafael <oliveirarafael22@outlook.com.br>
2025-05-12 03:05:02 -0300

    refactor: remove unused create method from PaymentController; update delete method in PaymentPolicy to check user roles

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -30,13 +30,7 @@ class PaymentController extends Controller
 
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
+
 
     /**
      * Store a newly created resource in storage.
@@ -151,6 +145,6 @@ class PaymentController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $this->authorize('delete', Payment::class);
     }
 }

88f6f25715116c8079d00b8ce22bd288ec99e148
Rafael Delgado <oliveirarafael22@outlook.com.br>
2025-05-17 13:51:02 -0300

    refactor: implementando a logica para verificar a origem de login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\ClassesAuthRequest;
 use App\Models\Student;
+use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -11,11 +12,112 @@ use function Pest\Laravel\json;
 
 class AuthController extends Controller
 {
+    private const ROLES = [
+        'ADMIN' => 'admin',
+        'TEACHER' => 'teacher'
+    ];
     public function login(ClassesAuthRequest $request)
     {
+//        $request->validated();
+//        $applicationSource = $request->header('X-Application-Source');
+//        if(empty($applicationSource)){
+//            return response()->json([
+//                'message' => 'Application source header is required'
+//            ], 400);
+//        }
+//
+//
+//
+//        if (Auth::attempt($request->all())) {
+//           $user = Auth::user();
+//           $token = $user->createToken('auth_token')->plainTextToken;
+//           $isStudent = Student::where('users_id',$user->id)->exists();
+//
+//            if ($isStudent)
+//            {
+//                return response()->json([
+//                    'access_token' => $token,
+//                    'token_type' => 'Bearer',
+//                    'user'=>[
+//                        'id' => $user->id,
+//                        'name'=> $user->name,
+//                        'CompleteStudentRecord'=>$isStudent
+//                    ]
+//                ]);
+//            }else
+//            {
+//                return response()->json([
+//                    'access_token' => $token,
+//                    'token_type' => 'Bearer',
+//                    'user'=>[
+//                        'id' => false,
+//                        'name'=> $user->name,
+//                        'CompleteStudentRecord'=>$isStudent
+//                    ]
+//                ]);
+//            }
+//
+//
+//        }
+//        return  response()->json([
+//            'message' => 'Unauthorized'
+//        ], 401);
+
+
+
         $request->validated();
-//        $credentials = $request->only('email', 'password');
-//            dd($request->all());
+
+        if (!$this->validateApplicationSource($request)){
+            return $this->errorResponse('Application source header is required', 400);
+        }
+
+//        dd($request->only('email'));
+        $isExistsUser = User::where('email', $request->email)->exists();
+        if ($isExistsUser === false){
+            return response()->json([
+                'message' => 'User not found'
+            ], 404);
+        }
+        if (Auth::attempt($request->only('email', 'password')) === false){
+            return response()->json([
+                'message' => 'Unauthorized'
+            ], 401);
+        }
+
+        if ($applicationSource === 'dashboard') {
+            $user = Auth::user();
+//            $verifiedUser = User::whereId($user->id)
+//                ->whereIn('role', [self::ROLES['ADMIN'], self::ROLES['TEACHER']])
+//                ->exists();
+            if ($this->isAdmin($user)){
+                return response()->json([
+                        'message' => 'Unauthorized'
+                    ], 401);
+
+            }
+            return response()->json([
+                'message' => 'Login successful',
+                'user' => [
+                    'id' => $user->id,
+                    'name' => $user->name,
+                    'role' => $this->isAdmin($user)
+                ]
+            ]);
+
+//            $token = $user->createToken('auth_token')->plainTextToken;
+//            $isStudent = Student::where('users_id', $user->id)->exists();
+//            return response()->json([
+//                'access_token' => $token,
+//                'token_type' => 'Bearer',
+//                    'user' => [
+//                        'id' => $user->id,
+//                        'name' => $user->name,
+//                        'CompleteStudentRecord' => $isStudent
+//                    ]
+//            ]);
+
+        }
+
         if (Auth::attempt($request->all())) {
            $user = Auth::user();
            $token = $user->createToken('auth_token')->plainTextToken;
@@ -44,13 +146,32 @@ class AuthController extends Controller
                     ]
                 ]);
             }
-
-
         }
         return  response()->json([
             'message' => 'Unauthorized'
         ], 401);
+
+
+
+
+
     }
+    private function validateApplicationSource(Request $request): bool
+    {
+        return !empty($request->header('X-Application-Source'));
+    }
+   private function isAdmin(User $user): string
+   {
+
+         return $user->role === self::ROLES['ADMIN'] ? 'admin' : 'teacher';
+   }
+
+    private function errorResponse(string $message, int $status)
+    {
+        return response()->json(['message' => $message], $status);
+    }
+
+
     public function logout(Request $request)
     {
 

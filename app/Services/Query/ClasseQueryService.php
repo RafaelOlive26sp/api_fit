3b84a0796692a7c6a7ec48559748951e1a271907
Rafael Delgado <oliveirarafael22@outlook.com.br>
2025-05-21 12:43:03 -0300

    Foi criado o novo Service para tratar as consultar ao model Classe, todas as consultas relacionas a este model.

--- app/Services/Query/ClasseQueryService.php
+++ app/Services/Query/ClasseQueryService.php
@@ -1,1 +1,40 @@
 <?php
+
+namespace App\Services\Query;
+
+use App\Models\Classe;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Collection;
+
+
+class ClasseQueryService
+{
+    /**
+     * Get all classes.
+     *
+     * @return Collection
+     */
+    public function getAllClasses(): Collection
+    {
+        return Classe::with([
+            'schedulesPatterns',
+            'extraClasses.classe',
+            'user'=> function ($query) {
+                $query->select('id', 'name');
+            },
+        ])->select('id', 'name', 'max_students', 'level')->get();
+
+
+    }
+
+    /**
+     * Get class by ID.
+     *
+     * @param int $id
+     * @return Classe|null
+     */
+    public function getClassById(int $id): ?Classe
+    {
+        return Classe::find($id);
+    }
+}

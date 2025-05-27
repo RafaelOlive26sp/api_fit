f221fc98b5fd5d173648ba46b6b44f9cb6c42038
Rafael Delgado <oliveirarafael22@outlook.com.br>
2025-05-26 21:27:30 -0300

    Feat: implementado para dividir responsabilidades e com uso de boas praticas, este metodo fica responsavel pela atualização aonde esta recebendo os dados do DTO e em conjunto fazendo as buscas na Query responsavel e retornando os dados

--- app/Services/PaymentService.php
+++ app/Services/PaymentService.php
@@ -1,1 +1,29 @@
 <?php
+
+namespace App\Services;
+
+use App\DTO\UpdatePaymentDTO;
+use App\Services\Query\PaymentQueryService;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+
+
+class PaymentService
+{
+    public function __construct(
+        protected PaymentQueryService $paymentQueryService
+    ){}
+
+
+    public function update(UpdatePaymentDTO $updatePaymentDTO, string $id): void
+    {
+
+        $payment = $this->paymentQueryService->findDataPaymentUserId($id);
+
+        if(!$payment){
+            throw new ModelNotFoundException("Pagamento não encontrado.");
+        }
+        $payment->update($updatePaymentDTO->toArray());
+    }
+
+
+}

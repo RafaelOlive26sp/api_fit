55912afbd7928ee758b17887ac817f87abe10af7
Rafael Delgado <oliveirarafael22@outlook.com.br>
2025-06-01 19:30:13 -0300

    Feat: Arquivo responsavel para fazer toda a logida de consulta ao banco de dados

--- app/Services/Query/ScheduleClassesQueryService.php
+++ app/Services/Query/ScheduleClassesQueryService.php
@@ -1,1 +1,44 @@
 <?php
+
+namespace App\Services\Query;
+
+use App\Models\ClassSchedulesPattern;
+use Illuminate\Database\Eloquent\Collection;
+
+
+class ScheduleClassesQueryService
+{
+    /**
+     * Get all class schedules patterns.
+     *
+     * @return \Illuminate\Database\Eloquent\Collection
+     */
+    public function getAllClassSchedulesPatterns()
+    {
+        return ClassSchedulesPattern::with([
+            'classe',
+            'classSchedule'
+        ])->get();
+    }
+
+    /**
+     * Get a class schedule pattern by ID.
+     *
+     * @param int $id
+     * @return ClassSchedulesPattern|null
+     */
+    public function getClassSchedulePatternById(int $id): ?ClassSchedulesPattern
+    {
+        return ClassSchedulesPattern::with([
+            'classe',
+            'classSchedule'
+        ])->findOrFail($id);
+    }
+    public function ifExistsClassSchedulePatternById(int $id): bool
+    {
+        return ClassSchedulesPattern::where('id', $id)->exists();
+    }
+}
+{
+
+}

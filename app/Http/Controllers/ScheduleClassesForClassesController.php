973dbbf15bf0475eb8b9fa74a00d0d076fdb3b6a
Rafael Delgado <oliveirarafael22@outlook.com.br>
2025-06-01 19:33:05 -0300

    Refactor: o metodo Store for refactorado para a inclusao de DTO ea funcao privada afim de ter mais manutenbilidade e claresa do codigo

--- app/Http/Controllers/ScheduleClassesForClassesController.php
+++ app/Http/Controllers/ScheduleClassesForClassesController.php
@@ -3,58 +3,30 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ScheduleClassesForClassesRequest;
-use App\Models\Appointment;
+//use App\Models\Appointment;
 use App\Models\ClassSchedulesPattern;
+use App\Services\ScheduleClassesFClassesService;
 use App\Models\User;
 use Illuminate\Http\Request;
+use App\DTO\ScheduleClassesFClassesDTO\StoreScheduleClassesDTO;
 
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 class ScheduleClassesForClassesController extends Controller
 {
+    public function __construct(
+        protected ScheduleClassesFClassesService $scheduleClassesFClassesService,
+    ){}
+
     use AuthorizesRequests;
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        $this->authorize('viewAny', User::class);
-
-       return 'estamos em manutenção';
-    }
-
-
-
     /**
      * Store a newly created resource in storage.
      */
     public function store(ScheduleClassesForClassesRequest $request)
     {
         $this->authorize('create', User::class);
-        $validateData = $request->validated();
-        ClassSchedulesPattern::create($validateData);
-
+        $this->validateAndCreateSchedule($request->validated());
         return response()->json(['message' => 'aula criada com sucesso!'], 201);
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        $this->authorize('viewAny', User::class);
-        return 'estamos em manutenção';
-    }
-
-
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        $this-> authorize('update', User::class);
-        return 'estamos em manutenção';
     }
 
     /**
@@ -65,4 +37,12 @@ class ScheduleClassesForClassesController extends Controller
       $this->authorize('delete', User::class);
         return 'estamos em manutenção';
     }
+
+    private function validateAndCreateSchedule($request)
+    {
+        $DTO = StoreScheduleClassesDTO::fromRequest($request);
+        $this->scheduleClassesFClassesService->store($DTO);
+    }
+
+
 }

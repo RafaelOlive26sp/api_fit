a2e79fb567fca91f46c360e734654c1025cc250c
Rafael Delgado <oliveirarafael22@outlook.com.br>
2025-06-08 11:04:55 -0300

    Feat: Este service é responsavel para a criação da aula, obs o usuario podera criar diversos agendamentos

--- app/Services/ScheduleService.php
+++ app/Services/ScheduleService.php
@@ -1,1 +1,20 @@
 <?php
+
+namespace App\Services;
+
+
+use App\DTO\ScheduleControllersDTO\StoreScheduleControllerDTO;
+use App\Models\StudentClass;
+
+
+class ScheduleService
+{
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreScheduleControllerDTO $storeScheduleControllerDTO): void
+    {
+        // o usuario pode criar diversas turmas
+        StudentClass::create($storeScheduleControllerDTO->toArray());
+    }
+}
